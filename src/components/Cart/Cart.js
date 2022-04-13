8e52ae8c2562f2763793335a230e057548d105c4
lvivas <vivasleonardo4@gmail.com>
2022-04-13 00:09:45 -0300

    clase 9

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,39 +1,41 @@
-import React from "react";
+import React, {useContext} from "react";
 import CartContext from "../../Contex/CartContex";
-const Cart = ( () => {
+const Cart = (() => {
 
     const { cartProducts, deleteOne } = useContext(CartContext)
+
+
+    
     return (
         <>
-        <h2> Hola Mundo </h2>
-        <br/>
-        <button>eliminar productos</button>
+            <h2> Hola Mundo </h2>
+            <br />
 
+                {
+                    
+                    cartProducts.map((prod) => ( 
+                    
+                        <div className="card-item-dos" key = {prod.id}>
+                            <div className="container__box">
 
-        <>
-            
-              {
-                  cartProducts.map((prod))
-              }  
-            <div className="card-item-dos" onClick={chengePage}>
-                <div className="container__box">
-                       
-                    <div className="box">
-                        <img src= {image} alt="..." />
+                                <div className="box">
+                                    <img src={prod.image} alt="..." />
 
-                        <button onClick={addToCart}>Comprar</button>
-                        {/* <ItemCount stock={stock} initial={1} onAdd={onAdd} addToCart={addToCart}/> */}
-                    <div className="detail-card">
-                        <h4>{titel}</h4>
-                        <h4>${price}</h4>
+                                    <button onClick={() => deleteOne (prod.id)}>eliminar</button>
+                                    {/* <ItemCount stock={stock} initial={1} onAdd={onAdd} addToCart={addToCart}/> */}
+                                    <div className="detail-card">
+                                        <h4>{prod.titel}</h4>
+                                        <h4>${prod.price}</h4>
 
-                    </div>
-                    </div>
-                       
-                </div>
-            </div>
-       
-    </>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        
+                    ))
+
+                }     
         </>
     )
 })
